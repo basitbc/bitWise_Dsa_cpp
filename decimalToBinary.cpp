5c27e0a5dd0b3bddfce913668a03be097eb92847
basitmarvelminds <basit.marvelminds@gmail.com>
2023-02-18 22:47:23 +0530

    decimalToBinary

--- decimalToBinary.cpp
+++ decimalToBinary.cpp
@@ -5,16 +5,16 @@ using namespace std;
 void toBinary(int N)
 {
     // your code here
-    int ans = 0;
+    int answer = 0;
     int a = 0;
     while (N != 0)
     {
         int bit = N & 1;
         N = N >> 1;
-        ans = (pow(10, a) * bit + ans);
+        answer = (pow(10, a) * bit + answer);
         a++;
     }
-    cout << ans;
+    cout << answer;
 }
 
 int main()
